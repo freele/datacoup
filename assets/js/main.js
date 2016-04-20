dc4ee2bd81f54b0d561744c46b718a79884849de
Alexander Kremenets <kremenets0207@gmail.com>
2016-04-20 17:06:16 +0300

    Markup in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,6 @@ console.log('test');
 function expandCat(cat) {
   console.log('expandCat: ', cat);
   var cat = $(cat);
-  debugger;
-  $(cat).toggle('slow');
+  // debugger;
+  $(cat).toggle(400);
 }
