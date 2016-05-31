aff9c85999147408ebb750546127050b2e5bb49d
Alexander Kremenets <kremenets0207@gmail.com>
2016-05-31 16:19:37 +0300

    Browser navigation implementod, Url connection

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,66 @@ console.log('test');
 
 var selected = 'project';
 
-function expandCat(cat) {
-  console.log('expandCat: ', cat);
-  var cat = $(cat);
-  // debugger;
-  $(cat).toggle(400);
+function updateQueryWithoutReload(newQuery) {
+  if (history.pushState) {
+    var newUrl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + newQuery;
+    window.history.pushState({path:newUrl},'',newUrl);
+  }
 }
 
+function toggle(element, state) {
+  if (state === true) {
+    $(element).show(400);
+  } else if (state === false) {
+    $(element).hide(400);
+  } else {
+    $(element).toggle(400); 
+  }
+}
+
+function expandCat(cat, state, initialLoad) {
+  console.log('expandCat: ', cat);
+
+  if (!initialLoad) {
+    var openTabs = $.deparam(window.location.search.substr(1))
+    if (openTabs[cat] === undefined || openTabs[cat] === '0') {
+      openTabs[cat] = '1';
+    } else {
+      openTabs[cat] = '0';
+    }
+    lastOpenTabs = openTabs;
+
+    var newQuery = $.param(openTabs);
+    updateQueryWithoutReload(newQuery);
+  }
+
+  var cat = $('#' + cat);
+  toggle(cat, state);
+}
+
+
+var lastOpenTabs = {};
+var openTabs = {};
+function initFromUrl() {
+  openTabs = $.deparam(window.location.search.substr(1));
+  var initialLoad = true;
+  Object.keys(openTabs).forEach(function(key){
+    if (openTabs[key] === '1'){
+      expandCat(key, true, initialLoad);
+    } else if (openTabs[key] === '0') {
+      expandCat(key, false, initialLoad);
+    }
+  });
+  Object.keys(lastOpenTabs).forEach(function(key){
+    if (lastOpenTabs[key] === '1' && !openTabs[key]){
+      expandCat(key, false, initialLoad);
+    }
+  });
+}
+
+
+window.onpopstate = function(event) {
+  initFromUrl();
+};
+
+initFromUrl();
